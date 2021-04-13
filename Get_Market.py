a9a84c7fe5af330e6dc0e7adcf1cf1fc75f57423
Jerren Fair <75046397+j-fair@users.noreply.github.com>
2021-04-12 23:36:39 -0400

    load into Postgres
    Updated script to load both dataframes into Postgres

--- Get_Market.py
+++ Get_Market.py
@@ -2,6 +2,7 @@ import urllib.request
 import html2text
 import os
 import pandas as pd
+from sqlalchemy import create_engine
 
 webUrl  = urllib.request.urlopen('http://subopt.org/priceseth.txt')
 
@@ -20,11 +21,20 @@ create = open(newfile, "w")
 create.write(str(data))
 create.close()
 
-#Pandas dataframe for CSV
+#Pandas dataframe cleanup, and exporting to CSV
 market_df = pd.read_csv("market_data.txt", sep= ' ', lineterminator='\\', names = ['Date', 'Time', 'ID', 'Price ETH'])
+punks_df = pd.read_csv("PunkList.csv")
 
 market_df['Date'] = market_df['Date'].str[1:]
 market_df['Date'] = market_df['Date'].str.replace(r"[\"\',]",'')
 market_df['Price ETH'] = market_df['Price ETH'].str.replace(r"[\"\',]",'')
 
 market_df.to_csv('Market_Data.csv',  index = None)
+
+# Loading into Postgres
+engine = create_engine('postgresql://postgres:ENTERPASSWORD@localhost:5432/CryptoPunks')
+#ENTERPASSWORD
+conn = engine.connect()
+
+market_df.to_sql('market_df', engine)
+punks_df.to_sql('punks_df', engine)
